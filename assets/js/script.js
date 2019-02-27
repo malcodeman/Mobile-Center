4b91aef37449f62cf9355f3a94a6dbb687f42814
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-02-27 21:39:02 +0100

    Adds dynamic copyright year

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const copyrightYearMobile = document.getElementById("copyrightYearMobile");
+const copyrightYearDesktop = document.getElementById("copyrightYearDesktop");
+
+copyrightYearMobile.textContent = new Date().getFullYear();
+copyrightYearDesktop.textContent = new Date().getFullYear();
